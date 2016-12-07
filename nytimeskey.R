a90fdad6e7f1b756fe53ed87f65a8cb4b951965d
Clark <micl@UMICH.EDU>
2016-12-07 14:21:32 -0500

    initial commit after move

---
+++ nytimeskey.R
@@ -0,0 +1,4 @@
+# options(nytimes_cg_key = "69097fd7c483402e850fc626e0935e70")
+# options(nytimes_cf_key = "69097fd7c483402e850fc626e0935e70")
+options(nytimes_as_key = "69097fd7c483402e850fc626e0935e70")
+api_key = '69097fd7c483402e850fc626e0935e70'
